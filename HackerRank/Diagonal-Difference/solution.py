cd474b6b9d916bd69991e9fc2c5ab1201d4d3522
Adarsh Biradar <129952645+ARBiradar@users.noreply.github.com>
2026-07-07 17:00:48 +0530

    Add HackerRank solution: Diagonal Difference

---
+++ HackerRank/Diagonal-Difference/solution.py
@@ -0,0 +1,29 @@
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv
+        ("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<List<Integer>> arr = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            String[] arrRowTempItems = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+            List<Integer> arrRowItems = new ArrayList<>();
+
+            for (int j = 0; j < n; j++) {
+                int arrItem = Integer.parseInt(arrRowTempItems[j]);
+                arrRowItems.add(arrItem);
+            }
+
+            arr.add(arrRowItems);
+        }
+
+        int result = Result.diagonalDifference(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
