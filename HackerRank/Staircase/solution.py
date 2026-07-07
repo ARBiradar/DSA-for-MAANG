d49b35b018e81ae6f5ffc2429eacd838e0bd739d
Adarsh Biradar <129952645+ARBiradar@users.noreply.github.com>
2026-07-07 17:18:33 +0530

    Add HackerRank solution: Staircase

---
+++ HackerRank/Staircase/solution.py
@@ -0,0 +1,31 @@
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static void staircase(int n) {
+    // Write your code here
+
+    }
+
+}
+    for(int row = 0; row< n; row++){
+        for(int col = 0; col < n-row-1; col++){
+    }
+            System.out.print(" ");
+        }
+        System.out.println();
+        for(int col = 0; col <=row; col++){
+            System.out.print("#");
+        }
+     *
+     * Complete the 'staircase' function below.
+    /*
+
+
+class Result {
+import java.util.regex.*;
+import java.util.concurrent.*;
+import java.text.*;
+import java.util.*;
+import java.security.*;
+import java.math.*;
+import java.io.*;
